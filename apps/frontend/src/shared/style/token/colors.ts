18dcb98b5c5631158640c55778ba6fb408c87582
Oliy <eunsoa0310@gmail.com>
2025-12-24 04:45:07 +0900

    feat: color 토큰 추가

---
+++ apps/frontend/src/shared/style/token/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  // Grey Scale
+  grey900: '#000000',
+  grey800: '#242424',
+  grey700: '#434343',
+  grey600: '#656565',
+  grey500: '#898989',
+  grey400: '#AFAFAF',
+  grey300: '#D6D6D6',
+  grey200: '#F7F7F7',
+  grey100: '#FFFFFF',
+
+  // Primary
+  primary: '#3B82F6',
+  primaryHover: '#2563EB',
+  primaryPressed: '#1D4ED8',
+
+  // Chart
+  chartHigh: '#C8E6C9',
+  chartMid: '#FFCCBC',
+  chartLow: '#E1BEE7',
+} as const
