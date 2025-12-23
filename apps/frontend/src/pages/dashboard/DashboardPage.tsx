4d66421c3c7d0da5fe46d07ad3a8b8cc22d876ac
Oliy <eunsoa0310@gmail.com>
2025-12-24 06:44:07 +0900

    feat: 대시보드 페이지에 PurchaseSection 및 CustomerSection 추가

--- apps/frontend/src/pages/dashboard/DashboardPage.tsx
+++ apps/frontend/src/pages/dashboard/DashboardPage.tsx
@@ -1,3 +1,19 @@
+import { PurchaseSection } from './PurchaseSection/PurchaseSection'
+import { CustomerSection } from './CustomerSection/CustomerSection'
+import { NavigationBar, Spacing, Border } from '@shared/style'
+
 export function DashboardPage() {
-  return <div>DashboardPage</div>
+  return (
+    <div>
+      <NavigationBar title="📊 쇼핑몰 구매 데이터 대시보드" description="7월 구매 데이터 기반 분석 결과" />
+
+      <PurchaseSection />
+      <Spacing height={20} />
+
+      <Border height={20} />
+
+      <CustomerSection />
+      <Spacing height={20} />
+    </div>
+  )
 }
