eef23d543c055de25e3fccd0b8616e60998c4b1f
Oliy <eunsoa0310@gmail.com>
2025-12-24 06:43:56 +0900

    feat: 고객 목록을 표시하는 CustomerSection 컴포넌트 추가

---
+++ apps/frontend/src/pages/dashboard/CustomerSection/CustomerSection.tsx
@@ -0,0 +1,14 @@
+import { ListHeader } from '@shared/style'
+import { CustomerTableSection } from '@features/customer/components/CustomerTableSection/CustomerTableSection'
+
+export function CustomerSection() {
+  return (
+    <>
+      <ListHeader
+        title="👤 가장 많이 구매한 고객"
+        description="고객 목록을 정렬하여 확인할 수 있습니다. 고객 행을 클릭하면 해당 고객의 상세 구매 내역을 확인할 수 있습니다."
+      />
+      <CustomerTableSection />
+    </>
+  )
+}
