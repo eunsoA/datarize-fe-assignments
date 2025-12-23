2a63a911e2c6576a5a5d74901373864407d4c44e
Oliy <eunsoa0310@gmail.com>
2025-12-23 17:25:59 +0900

    feat: customerPurchase 타입 추가

--- apps/frontend/src/features/customer/customer.types.ts
+++ apps/frontend/src/features/customer/customer.types.ts
@@ -7,3 +7,14 @@ export type Customer = {
   count: number
   totalAmount: number
 }
+
+/**
+ * 고객 구매 내역 타입
+ */
+export type CustomerPurchase = {
+  date: string
+  quantity: number
+  product: string
+  price: number
+  imgSrc: string
+}
